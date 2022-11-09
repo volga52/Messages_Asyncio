8e6584ff0f055465d91dcecbf774e45ab72d47af
Евгений <volga52@bk.ru>
2022-11-09 14:29:20 +0300

    GUI start implementation

--- server/run.py
+++ server/run.py
@@ -1,8 +1,13 @@
 from argparse import ArgumentParser
-from asyncio import get_event_loop
+from asyncio import get_event_loop, set_event_loop
+
+from PyQt5 import Qt
+from sys import argv
+from quamash import QEventLoop
 
 from server.server_config import DB_PATH, PORT
 from server.utils.server_proto import ChatServerProtocol
+from server.ui.windows import ServerMonitorWindow
 
 
 class ConsoleServerApp:
@@ -46,6 +51,47 @@ class ConsoleServerApp:
         loop.close()
 
 
+class GuiServerApp:
+    """GUI server"""
+
+    def __init__(self, parsed_args, db_path):
+        self.args = parsed_args
+        self.db_path = db_path
+        self.ins = None
+
+    def main(self):
+        connections = dict()
+        users = dict()
+
+        # Each client will create a new protocol instance
+        self.ins = ChatServerProtocol(self.db_path, connections, users)
+
+        # GUI
+        app = Qt.QApplication(argv)
+        loop = QEventLoop(app)
+        # New must set the event loop
+        set_event_loop(loop)
+        wind = ServerMonitorWindow()
+        wind.show()
+
+        with loop:
+            coro = loop.create_server(lambda: self.ins,
+                                      self.args["addr"],
+                                      self.args["port"])
+            server = loop.run_until_complete(coro)
+
+            # Server requests until Ctrl+C
+            print('Serving on {} {}'.format(*server.sockets[0].getsockname()))
+            try:
+                loop.run_forever()
+            except KeyboardInterrupt:
+                pass
+
+            server.close()
+            loop.run_until_complete(server.wait_closed())
+            loop.close()
+
+
 def parse_and_run():
     def parse_args():
         parser = ArgumentParser(description="Server setting")
@@ -59,6 +105,10 @@ def parse_and_run():
     if args['nogui']:
         # start console server
         a = ConsoleServerApp(args, DB_PATH)
+        a.main()
+    else:
+        # Start GUI server
+        a = GuiServerApp(args, DB_PATH)
         a.main()
 
 
