dfce598553591eb9136d2a20429bce86ee23efa3
Евгений <volga52@bk.ru>
2022-11-14 20:17:39 +0300

    add messages in client_messages.py

--- client/utils/client_messages.py
+++ client/utils/client_messages.py
@@ -5,29 +5,20 @@ class JimClientMessage:
     def auth(self, username, password):
         """
         Authorization message
-        :param user:
-        :param password:
+        :param username: имя пользователя
+        :param password: пароль
         :return: dict with data
         """
         data = {
             'action': 'authenticate',
             'time': datetime.now().timestamp(),
-            # 'type': 'status',
+            # 'type': 'auth',
             'user': {
                 'account_name': username,
                 'password': password
             }
         }
         return data
-
-    # def response(self, code=None, error=None):
-    #     _data = {
-    #         'action': 'response',
-    #         'code': code,
-    #         'time': datetime.now(),
-    #         'error': error
-    #     }
-    #     return _data
 
     def presence(self, sender, status="Yep, I am here!"):
         """
@@ -47,3 +38,59 @@ class JimClientMessage:
         }
         return data
 
+    def quit(self, sender, status='disconnect'):
+        """
+        Сообщение пользователя о выходе
+        :param sender: username
+        :param status: тип сообщения
+        :return: словарь с данными о выходе
+        """
+        data = {
+            'action': 'quit',
+            'time': datetime.now().timestamp(),
+            'type': 'status',
+            'user': {
+                'account_name': sender,
+                'status': status
+            }
+        }
+        return data
+
+    def list_(self, sender, status='show', person=''):
+        """
+        Сообщение запрос на весь список контактов
+        :param sender: user_name
+        :param status: тип запроса
+        :param person: имя
+        :return: словарь с данными
+        """
+        data = {
+            'action': 'list',
+            'time': datetime.now().timestamp(),
+            'type': 'status',
+            'contact_list': 'No contact yet',
+            'user': {
+                'account_name': sender,
+                'status': status,
+                'contacts': person
+            }
+        }
+        return data
+
+    def message(self, sender, receiver='user1', text='some messages text'):
+        """
+        Пересылаемое сообщение
+        :param sender: получатель
+        :param receiver: отправитель
+        :param text: текст сообщения
+        :return: словарь с данными сообщения
+        """
+        data = {
+            'action': 'message',
+            'time': datetime.now().timestamp(),
+            'to': receiver,
+            'from': sender,
+            'encoding': 'utf-8',
+            'message': text
+        }
+        return data
