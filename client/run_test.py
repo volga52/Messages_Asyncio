c10853edae17674b65d48a6ec766a25cc2d2022e
Евгений <volga52@bk.ru>
2022-12-02 13:23:37 +0300

    Improved work with the database

---
+++ client/run_test.py
@@ -0,0 +1,159 @@
+from argparse import ArgumentParser
+from asyncio import ensure_future, get_event_loop, run, create_task, \
+    set_event_loop
+from sys import argv
+from sys import exit as sys_exit
+
+from PyQt5 import Qt, QtWidgets
+# from PyQt5 import QtWidgets
+
+from quamash import QEventLoop
+
+from client.ui.windows import LoginWindow, ContactsWindow
+from client.utils.client_proto_test import ChartClientProtocol, ClientAuth
+from client.client_config import DB_PATH, PORT
+
+
+class ConsoleClientApp:
+    """Console client"""
+
+    def __init__(self, parsed_args, db_path):
+        self.args = parsed_args
+        self.db_path = db_path
+        self.ins = None
+
+    def main(self):
+        # create event loop (текущий цикл событий)
+        loop = get_event_loop()
+
+        # authentication process
+        auth = ClientAuth(db_path=self.db_path)
+        while True:
+            _user = self.args["user"] or input("username: ")
+            password = self.args["password"] or input("password")
+            auth.username = _user
+            auth.password = password
+            is_auth = auth.authenticate()
+            if is_auth:
+                break
+            else:
+                print("wrong username/password")
+
+        tasks = []
+        _client = ChartClientProtocol(db_path=self.db_path,
+                                      loop=loop,
+                                      username=_user,
+                                      password=password)
+        try:
+            coro = loop.create_connection(lambda: _client, self.args["addr"],
+                                          self.args["port"])
+            transport, protocol = loop.run_until_complete(coro)
+
+        except ConnectionRefusedError:
+            print('Error. wrong server')
+            sys_exit(1)
+            # exit(1)
+
+        try:
+            task = loop.create_task(_client.get_from_console())
+            tasks.append(task)
+            loop.run_until_complete(task)
+        except KeyboardInterrupt:
+            pass
+        except Exception as e:
+            print(e)
+
+        finally:
+            loop.close()
+
+
+class GuiClientApp:
+    """GUI client"""
+    def __init__(self, parsed_args, db_path):
+        self.args = parsed_args
+        self.db_path = db_path
+        self.ins = None
+
+    def main(self):
+        """Основная функция запуска GUI client"""
+        # create event loop
+        # app = Qt.QApplication(argv)
+        app = QtWidgets.QApplication(argv)
+        loop = QEventLoop(app)
+        # New must set the event loop
+        set_event_loop(loop)
+
+        # authentication process
+        auth_ = ClientAuth(db_path=self.db_path)
+        login_window = LoginWindow(auth_instance=auth_)
+
+        # Отлов подтверждения
+        if login_window.exec_() == QtWidgets.QDialog.Accepted:
+            # Здесь можно вмешаться и изменить имя self.db_path
+            # Each client will create a new protocol instance
+            client_ = ChartClientProtocol(db_path=self.db_path,
+                                          loop=loop,
+                                          username=login_window.username,
+                                          password=login_window.password)
+            # Create Contacts window
+            window = ContactsWindow(client_instance=client_,
+                                    user_name=login_window.username)
+            # reference from protocol to GUI, for msg update
+            # ссылка из протокола на графический интерфейс пользователя
+            # для обновления msg
+            client_.gui_instance = window
+
+            with loop:
+                # cleaning old instances
+                del auth_
+                del login_window
+
+                # connect to our server
+                try:
+                    coro = loop.create_connection(lambda: client_,
+                                                  self.args['addr'],
+                                                  self.args['port'])
+                    server = loop.run_until_complete(coro)
+                except ConnectionRefusedError:
+                    print('Error. wrong server')
+                    exit(1)
+
+                # start GUI client
+                window.show()
+                # asyncio.ensure_future(client_.get_from_gui(loop))
+                # добавление истории
+                client_.get_from_gui()
+
+                # server requests until Ctrl+C
+                try:
+                    loop.run_forever()
+                except KeyboardInterrupt:
+                    pass
+                except Exception:
+                    pass
+
+
+def parse_and_run():
+    def parse_args():
+        parser = ArgumentParser(description="Client settings")
+        parser.add_argument("--user", default="user1", type=str)
+        parser.add_argument("--password", default="123", type=str)
+        parser.add_argument("--addr", default="127.0.0.1", type=str)
+        parser.add_argument("--port", default=PORT, type=int)
+        parser.add_argument('--nogui', action='store_true')
+        return vars(parser.parse_args())
+
+    args = parse_args()
+
+    if args['nogui']:
+        # start consoles server
+        a = ConsoleClientApp(args, DB_PATH)
+        a.main()
+    else:
+        # start GUI server
+        a = GuiClientApp(args, DB_PATH)
+        a.main()
+
+
+if __name__ == '__main__':
+    parse_and_run()
---
+++ client/utils/client_proto_test.py
@@ -0,0 +1,175 @@
+from asyncio import Protocol, CancelledError
+from binascii import hexlify
+from hashlib import pbkdf2_hmac
+from sys import stdout
+
+from client.utils.mixins import ConvertMixin, DbInterfaceMixin
+from client.utils.client_messages import JimClientMessage
+
+
+class ClientAuth(ConvertMixin, DbInterfaceMixin):
+    """Authentication server"""
+
+    def __init__(self, db_path, username=None, password=None):
+        super().__init__(db_path)
+        self.username = username
+        self.password = password
+
+    def authenticate(self):
+        """authentication method, which verify user in DB"""
+        # check user in DB
+        if self.username and self.password:
+            _user = self.get_client_by_username(self.username)
+            dk = pbkdf2_hmac('sha256', self.password.encode('utf-8'),
+                             'salt'.encode('utf-8'), 100000)
+            # Преобразование в 16-ричную строку
+            hashed_password = hexlify(dk)
+
+            if _user:
+                # existing user
+                if hashed_password == _user.password:
+                    # add client's history row
+                    self.add_client_history(self.username)
+                    return True
+                return False
+            else:
+                # new user
+                print('new user')
+                self.add_client(self.username, hashed_password)
+                # add client history row
+                self.add_client_history(self.username)
+                return True
+        else:
+            return False
+
+
+class ChartClientProtocol(Protocol, ConvertMixin, DbInterfaceMixin):
+    def __init__(self, db_path, loop, tasks=None, username=None, password=None,
+                 gui_instance=None, **kwargs):
+        super().__init__(db_path)
+        self.user = username
+        self.password = password
+        self.jim = JimClientMessage()
+        self.gui_instance = gui_instance
+        self.tasks = tasks
+
+        self.conn_is_open = False
+        self.loop = loop
+        self.sockname = None
+        self.transport = None
+        self.output = None
+
+    def connection_made(self, transport) -> None:
+        """Called when connection is initiated"""
+        self.sockname = transport.get_extra_info("sockname")
+        print(f"1 - {self.sockname}")
+
+        self.transport = transport
+        print(f"2 - {self.transport}")
+
+        self.send_auth(self.user, self.password)
+        self.conn_is_open = True
+
+    def connection_lost(self, exc):
+        """Метод отвечающий за корректное отключение пользователя"""
+        try:
+            self.conn_is_open = False
+            for task in self.tasks:
+                task.cancel()
+
+        except:
+            pass
+
+        finally:
+            self.loop.stop()
+            self.loop.close()
+
+    def send_auth(self, user, password):
+        """Send authenticate message to the server"""
+        if user and password:
+            self.transport.write(
+                self._dict_to_bytes(self.jim.auth(user, password)))
+
+    def data_received(self, data: bytes) -> None:
+        """
+        Received data from server and send output message to console/gui
+        :param data: json-like dict in bytes
+        :return: None
+        """
+        message = self._byte_to_dict(data)
+        if message:
+            try:
+                if message['action'] == 'probe':
+                    self.transport.write(self._dict_to_bytes(
+                        self.jim.presence(self.user,
+                                          status="Connected from {0} {1}".format(
+                                              *self.sockname))))
+                elif message['action'] == 'response':
+                    if message['code'] == 200:
+                        pass
+                    elif message['code'] == 402:
+                        self.connection_lost(CancelledError)
+                    else:
+                        self.output(message)
+
+                # elif message['action'] == 'msg':
+                elif message['action'] == 'message':
+                    self.output(message)
+
+            except Exception as exc:
+                print(exc)
+
+    async def get_from_console(self):
+        while not self.conn_is_open:
+            pass
+        self.output = self.output_to_console
+        self.output("{2} connected to {0}:{1}\n".format(
+            *self.sockname, self.user))
+
+        while True:
+            content = await self.loop.run_in_executor(None, input)
+            # print(content)
+            print(f"3 - {content}")
+
+    def output_to_console(self, data):
+        _data = data
+        stdout.write(_data)
+
+    def send(self, request):
+        """Функция непосредственной отправки сообщений"""
+        if request:
+            msg = self._dict_to_bytes(request)
+            self.transport.write(msg)
+
+    def send_msg(self, to_user, content):
+        """Функция отправки текстовых сообщений другому пользователю"""
+        if to_user and content:
+            request = self.jim.message(self.user, to_user, content)
+            self.transport.write(self._dict_to_bytes(request))
+            # Запись в БД отправляемого сообщение
+            # self.gui_instance.client_instance.add_client_message(
+            # client_username = self.user,  # от кого
+            # contact_username = to_user,  # кому
+            #     text_msg=content
+            # )
+
+    def get_from_gui(self):
+        self.output = self.output_to_gui
+
+    def output_to_gui(self, msg, response=False):
+        try:
+            if self.gui_instance:
+                if response:
+                    self.gui_instance.is_auth = True
+
+                if self.user == msg['to']:
+                    # Запись в БД пришедшее сообщение
+                    # self.gui_instance.client_instance.add_client_message(
+                    # client_username = msg['from'],  # от кого
+                    # contact_username = msg['to'],  # кому
+                    #     text_msg=msg['message']
+                    # )
+                    self.gui_instance.chat_ins()
+
+        except Exception as e:
+            print(e)
